c21a6bb3157ec301dbe327cd8edbb5993cbabc29
Kostiantyn Servetnyk <78149855+kistyak@users.noreply.github.com>
2022-09-14 20:22:04 +0300

    Remove unused fields

--- src/main/java/net/kistyak/tictac/HelloController.java
+++ src/main/java/net/kistyak/tictac/HelloController.java
@@ -1,18 +1,10 @@
 package net.kistyak.tictac;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button firstCenter;
