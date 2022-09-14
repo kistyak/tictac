8b6601ab79ae3cff79e9d9fcc5a2a48423888e1b
Kostiantyn Servetnyk <78149855+kistyak@users.noreply.github.com>
2022-09-14 20:20:29 +0300

    Write and add win-checker to buttons

--- src/main/java/net/kistyak/tictac/HelloController.java
+++ src/main/java/net/kistyak/tictac/HelloController.java
@@ -52,41 +52,53 @@ public class HelloController {
 
         firstUp.setOnAction(e -> {
             pressedButton(firstUp);
+            checker();
         });
 
         secondUp.setOnAction(e -> {
             pressedButton(secondUp);
+            checker();
         });
 
         thirdUp.setOnAction(e -> {
             pressedButton(thirdUp);
+            checker();
         });
 
         firstCenter.setOnAction(e -> {
             pressedButton(firstCenter);
+            checker();
         });
 
         secondCenter.setOnAction(e -> {
             pressedButton(secondCenter);
+            checker();
         });
 
         thirdCenter.setOnAction(e -> {
             pressedButton(thirdCenter);
+            checker();
         });
 
         firstDown.setOnAction(e -> {
             pressedButton(firstDown);
+            checker();
         });
 
         secondDown.setOnAction(e -> {
             pressedButton(secondDown);
+            checker();
         });
 
         thirdDown.setOnAction(e -> {
             pressedButton(thirdDown);
+            checker();
         });
         
         newGame.setOnAction(e -> {
+
+            winText.setText("");
+
             firstUp.setDisable(false);
             firstUp.setText("");
             secondUp.setDisable(false);
@@ -121,6 +133,95 @@ public class HelloController {
         }
         count++;
         button.setDisable(true);
+    }
+
+    public void checker(){
+
+        // For X
+        if(firstUp.getText().equals("X") && secondUp.getText().equals("X") && thirdUp.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+        if(firstCenter.getText().equals("X") && secondCenter.getText().equals("X") && thirdCenter.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+        if(firstDown.getText().equals("X") && secondDown.getText().equals("X") && thirdDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+
+        if(firstUp.getText().equals("X") && firstCenter.getText().equals("X") && firstDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+        if(secondUp.getText().equals("X") && secondCenter.getText().equals("X") && secondDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+        if(thirdUp.getText().equals("X") && thirdCenter.getText().equals("X") && thirdDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+
+        if(firstUp.getText().equals("X") && secondCenter.getText().equals("X") && thirdDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+        if(thirdUp.getText().equals("X") && secondCenter.getText().equals("X") && firstDown.getText().equals("X")){
+            winText.setText("X WINS!");
+            disableAll();
+        }
+
+        // For O
+        if(firstUp.getText().equals("O") && secondUp.getText().equals("O") && thirdUp.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+        if(firstCenter.getText().equals("O") && secondCenter.getText().equals("O") && thirdCenter.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+        if(firstDown.getText().equals("O") && secondDown.getText().equals("O") && thirdDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+
+        if(firstUp.getText().equals("O") && firstCenter.getText().equals("O") && firstDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+        if(secondUp.getText().equals("O") && secondCenter.getText().equals("O") && secondDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+        if(thirdUp.getText().equals("O") && thirdCenter.getText().equals("O") && thirdDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+
+        if(firstUp.getText().equals("O") && secondCenter.getText().equals("O") && thirdDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+        if(thirdUp.getText().equals("O") && secondCenter.getText().equals("O") && firstDown.getText().equals("O")){
+            winText.setText("O WINS!");
+            disableAll();
+        }
+    }
+
+    private void disableAll(){
+        firstUp.setDisable(true);
+        secondUp.setDisable(true);
+        thirdUp.setDisable(true);
+
+        firstCenter.setDisable(true);
+        secondCenter.setDisable(true);
+        thirdCenter.setDisable(true);
+
+        firstDown.setDisable(true);
+        secondDown.setDisable(true);
+        thirdDown.setDisable(true);
     }
 
 }
